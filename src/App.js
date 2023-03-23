49aa2426bae3a1a72582d896e626404b4fb1a112
tvoegerl <voegerlt@msu.edu>
2023-03-22 22:31:06 -0400

    react component lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import puppy from './puppy.jfif';
+
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false }
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {zine.title}
+    </li>
+    );
+    return (
+      <div>
+        <h3>Magazines</h3>
+        <ul>{listZines}</ul>
+      </div>
+    );
+}
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway', 
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {book.image &&
+      <img
+        className='bookCover'
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+        />
+      }
+    </div>
+  );
+}
+
+
+function MagicButton() {
+  return (
+    <div>
+      <h5>This is a magic button</h5>
+      <button>Magic</button>
+    </div>
+  );
+}
+
+function CutePuppy() {
+  return (
+    <div class="CutePup">
+      <p>Destress with a puppy pic!</p>
+      <img src={puppy} alt="adorable puppy"/>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Tatiana and MSU!
-        </a>
+        <Bookshelf/>
+        <ZineRack/>
+        <CutePuppy/>
+        <MagicButton/>
       </header>
+      
     </div>
   );
 }
